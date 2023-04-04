481d268f96f575668698d4525ee0d0e6b8cb0f6a
unknown <martin.lisi1212@gmail.com>
2023-04-03 22:42:52 -0300

    Ejercicio 1

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 const personajesSimpson = [
-    {nombre : "Homero", edad: 39, rol: "Padre"},
-    {nombre : "Marge", edad: 36, rol: "Madre"},
-    {nombre : "Bart", edad: 10, rol: "Hijo"},
-    {nombre : "Lisa", edad: 8, rol: "Hija"},
-    {nombre : "Maggie", edad: 1, rol: "Hija"},
-    {nombre : "Ned", edad: 35, rol: "Vecino"},
-    {nombre : "Ralph", edad: 8, rol: "Compañero de clase"},
-    {nombre : "Milhouse", edad: 10, rol: "Amigo"},
-    {nombre : "Nelson", edad: 12, rol: "Bravucon"},
-    {nombre : "Martin", edad: 10, rol: "Compañero de clase"},
+    { nombre: "Homero", edad: 39, rol: "Padre" },
+    { nombre: "Marge", edad: 36, rol: "Madre" },
+    { nombre: "Bart", edad: 10, rol: "Hijo" },
+    { nombre: "Lisa", edad: 8, rol: "Hija" },
+    { nombre: "Maggie", edad: 1, rol: "Hija" },
+    { nombre: "Ned", edad: 35, rol: "Vecino" },
+    { nombre: "Ralph", edad: 8, rol: "Compañero de clase" },
+    { nombre: "Milhouse", edad: 10, rol: "Amigo" },
+    { nombre: "Nelson", edad: 12, rol: "Bravucon" },
+    { nombre: "Martin", edad: 10, rol: "Compañero de clase" },
 ]
+
+const personajesMenores18 = personajesSimpson.filter(personaje => personaje.edad < 18)
+console.log(personajesMenores18);
+
+
+
