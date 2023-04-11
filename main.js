dc0c6146651877a129ed3c4ffe2619a38a2ba46f
unknown <martin.lisi1212@gmail.com>
2023-04-10 21:59:40 -0300

    Ejercicio 4

--- main.js
+++ main.js
@@ -24,18 +24,29 @@ const nuevoArray = personajesMenores18.map(personaje => personaje.nombre)
 console.log(nuevoArray);
 
 console.log("Ejercicio 4")
-/*
-const nuevoArrayEstudiantes = personajesMenores18.map(personaje => personaje )
-console.log(nuevoArrayEstudiantes);
-*/
-/*
-const array2 = personajesMenores18.map(personaje => personaje)
-const personaje = [uno, dos, tres, cuatro, cinco, seis, siete] = array2.rol;
-personaje.rol = "Estudiante";
 
-console.log(array2);
+//const cambioRolPersonajes = personajesMenores18.map(({ nombre, edad, rol }) => ({ nombre, edad, rol: "Estudiante" }));
+//console.log(cambioRolPersonajes);
+//const mapRolEjs1 = filtroMenores18.map(({ nombre, edad, rol }) => ({ nombre, edad, rol: "Estudiante" }));
+
+/*
+const cambiarRol = personajesMenores18.map(({ nombre, edad, rol }) => {
+    if (edad < 18) {
+        return { nombre, edad, rol: "Estudiante" };
+    } else {
+        return { nombre, edad, rol };
+    }
+});
+console.log(cambiarRol);
 */
 
+const personajesActualizados = personajesSimpson.map(personaje => {
+    const { edad, rol } = personaje;
+    return {
+        ...personaje, rol: edad < 18 ? "Estudiante" : rol
+    }
+})
+console.log(personajesActualizados);
 
 console.log("Ejercicio 5")
 const nuevosPersonajes = [
